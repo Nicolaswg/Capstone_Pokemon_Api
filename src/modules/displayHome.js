f13fb0daed3d79db858f24433224a765a135aa57
Reem <janinareem@gmail.com>
2022-02-03 21:07:49 +0200

    Update files

--- src/modules/displayHome.js
+++ src/modules/displayHome.js
@@ -1,35 +1,70 @@
-import comPopUp from './commentPopUp.js';
-import { postLike, getLike } from './likeApi.js';
-import itemCount from './counters.js';
+import { comPopUp, getCounter, getCommentArr } from "./commentPopUp.js";
+import { postLike, getLike } from "./likeApi.js";
+import itemCount from "./counters.js";
 
-const header = document.querySelector('.header');
-const pokedex = document.getElementById('pokedex');
+const header = document.querySelector(".header");
+const pokedex = document.getElementById("pokedex");
 const displayHeader = (arr) => {
   const pokeNum = itemCount(arr);
   const html = `<div class="logo"></div><ul class="nav-bar"><li><a href="#" class="nav-link">Pokemons (${pokeNum})</a></li><li><a href="#" class="nav-link">Game List</a></li><li><a href="#"class="nav-link">Players</a></li></ul>`;
   header.innerHTML = html;
 };
 const pokeCard = async (pokeArr) => {
-  let pokeHTML = '';
+  let pokeHTML = "";
   const lkNum = await getLike();
-  pokeArr.map((e, i) => {
-    pokeHTML += `<div class="pokemon" id="${pokeArr[i].id}"><div class="img-container"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokeArr[i].id}.png" class="poke-img"></div><div class="poke-title"><p class="poke-name">${pokeArr[i].name}</p><i class="far fa-heart lk"></i></div><div class="poke-like"><span class ="lk-counter">${lkNum[i].likes}</span><p class="likes">Likes</p></div><div class="btn-container"><button class="com-btn">Comments</button></div></div>`;
+  console.log(lkNum);
+  pokeArr.forEach((element, i) => {
+    pokeHTML += `<div class="pokemon" id="${pokeArr[i].id}">
+      <div class="img-container">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokeArr[i].id}.png" class="poke-img">
+      </div>
+      <div class="poke-title">
+      <p class="poke-name">${pokeArr[i].name}</p>
+      <i class="far fa-heart lk"></i></div>
+      <div class="poke-like">
+      <span class ="lk-counter">${lkNum[i].likes}</span>
+      <p class="likes">Likes</p></div>
+      <div class="btn-container">
+      <button class="com-btn">Comments</button>
+      </div>
+      </div>`;
     pokedex.innerHTML = pokeHTML;
-    return pokeArr;
   });
-  const likes = document.querySelectorAll('.lk');
-  const sapns = document.querySelectorAll('.lk-counter');
+  // console.log(pokeArr);
+  const likes = document.querySelectorAll(".lk");
+  const sapns = document.querySelectorAll(".lk-counter");
+  // console.log(likes);
   likes.forEach((like, i) => {
-    like.addEventListener('click', () => {
+    like.addEventListener("click", async () => {
       sapns[i].textContent = Number(sapns[i].textContent) + 1;
-      postLike(i);
+      await postLike(i);
+      const lk = await getLike();
+      console.log(i);
+      document.querySelector(".lk-counter").textContent = lk[i].likes;
     });
   });
-  const commentBtn = document.querySelectorAll('.com-btn');
+
+  const commentBtn = document.querySelectorAll(".com-btn");
   commentBtn.forEach((e, i) => {
-    commentBtn[i].addEventListener('click', (e) => {
-      const pokId = e.target.parentNode.id;
+    commentBtn[i].addEventListener("click", async (e) => {
+      const pokId = e.target.parentNode.parentNode.id;
       comPopUp(pokeArr, i, pokId);
+
+      document.querySelector(".counter").textContent = await getCounter(pokId);
+
+      const commentDiv = document.querySelector(".comments");
+      const comments = await getCommentArr(pokId);
+
+      comments.forEach((elemnt) => {
+        const div = document.createElement("div");
+        div.className = "comment";
+        const nameWithComment = document.createElement("p");
+        nameWithComment.className = "comment";
+        const title = `${elemnt.creation_date} ${elemnt.username} : ${elemnt.comment}`;
+        nameWithComment.textContent = title;
+        div.appendChild(nameWithComment);
+        commentDiv.appendChild(div);
+      });
     });
   });
   // const pokeName = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
--- src/modules/pokeRender.js
+++ src/modules/pokeRender.js
@@ -1,15 +1,18 @@
 /* eslint-disable no-await-in-loop */
-import List from './pokeList.js';
-import { displayHeader, pokeCard } from './displayHome.js';
+import List from "./pokeList.js";
+import { displayHeader, pokeCard } from "./displayHome.js";
+import { postLike } from "./likeApi.js";
 
-const pokemonNumber = 9;
+const pokemonNumber = 13;
 const pokemonList = async () => {
   for (let i = 1; i <= pokemonNumber; i += 1) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     const res = await fetch(url);
     const pokemon = await res.json();
     List.pokeList.push(pokemon);
+    // postLike(i); 
   }
+  console.log(List.pokeList);
   pokeCard(List.pokeList);
   displayHeader(List.pokeList);
   return List.pokeList;
