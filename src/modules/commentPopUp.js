b021f4692c374c460f23f6f3ff194b32f9fa55ac
Reem <janinareem@gmail.com>
2022-02-03 04:46:26 +0200

    Add functionalit of add comments from form

--- src/modules/commentPopUp.js
+++ src/modules/commentPopUp.js
@@ -1,6 +1,6 @@
 // const { DateTime } = require('luxon');
 
-const comPopUp = (pokemon, i) => {
+const comPopUp = (pokemon, i, pokId) => {
   const parent = document.createElement('div');
   parent.classList.add('modal-bg');
   parent.classList.add('overlay');
@@ -8,7 +8,7 @@ const comPopUp = (pokemon, i) => {
   commentContainer.classList.add('com-contain');
 
   let commentHtml = '';
-  commentHtml += `<section class="coment-sec">
+  commentHtml += `<section class="coment-sec" >
     <button class="close-popUp">X</button>
     <div class="img-div-contain">
        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemon[i].id}.png" class="poke-img"> 
@@ -20,14 +20,15 @@ const comPopUp = (pokemon, i) => {
         <p>type: ${pokemon[i].types[0].type.name}</p>
     </div>
     <div>
+       <p class="label2"> Comments () </p>  
         <div class="comments">
-                <p class="label2"> Comments () </p>  
+
         </div>
         <div class="Add-comment">
             <form class="form">
                 <label class="label">Add a comment ✍</label>
                 <input class="input-name" id="name" type="text" placeholder="Your name">
-                <textarea class="input-area" id="txtArea" cols="30" rows="10" placeholder="Your insights"></textarea>
+                <textarea class="input-area" id="txtArea" cols="8" rows="3" placeholder="Your insights"></textarea>
                 <button class="btn" type="submit">Comment</button>
             </form>
         </div>
@@ -43,6 +44,71 @@ const comPopUp = (pokemon, i) => {
   btnClose.addEventListener('click', () => {
     parent.remove();
   });
+
+  const inputName = document.querySelector('.input-name');
+  const inputArea = document.querySelector('.input-area');
+  const formDiv = document.querySelectorAll('.form');
+  const commentDiv = document.querySelector('.comments');
+
+  const getCommentArr = async (comId) => {
+    const response = await fetch(
+      `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ApHeiaIj7DwoNhyw7w7c/comments?item_id=${comId}`,
+    );
+    return response.json();
+  };
+
+  function displayComments(comments) {
+    comments.forEach((elemnt) => {
+      const div = document.createElement('div');
+      div.className = 'comment';
+      const nameWithComment = document.createElement('p');
+      nameWithComment.className = 'comment';
+      const title = `${elemnt.creation_date} ${elemnt.username} : ${elemnt.comment}`;
+      nameWithComment.textContent = title;
+      div.appendChild(nameWithComment);
+      commentDiv.appendChild(div);
+    });
+  }
+
+  const addElementsToPageFrom = async (comId) => {
+    await getCommentArr(comId).then((comments) => {
+      displayComments(comments);
+    });
+  };
+
+  const addCommentToArray = async (id, nametTxt, commentArea) => {
+    await fetch(
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ApHeiaIj7DwoNhyw7w7c/comments',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          item_id: `${id}`,
+          username: `${nametTxt}`,
+          comment: `${commentArea}`,
+        }),
+      },
+    );
+  };
+
+  for (let i = 0; i < formDiv.length; i += 1) {
+    formDiv[i].addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const pok = Number(pokId);
+      addElementsToPageFrom(pok);
+      const name = document.getElementById('name').value;
+      const comment = document.getElementById('txtArea').value;
+      addCommentToArray(pok, name, comment);
+      addElementsToPageFrom(pok);
+      inputName.value = '';
+      inputArea.value = '';
+      inputName.focus();
+    });
+  }
 };
 
 export default comPopUp;
--- src/modules/displayHome.js
+++ src/modules/displayHome.js
@@ -8,18 +8,19 @@ const displayHeader = () => {
   const html = `<div class="logo"></div><ul class="nav-bar"><li><a href="#" class="nav-link">Pokemons (${pokemonNumber})</a></li><li><a href="#" class="nav-link">Game List</a></li><li><a href="#"class="nav-link">Players</a></li></ul>`;
   header.innerHTML = html;
 };
-
 const pokeCard = async (pokeArr) => {
   let pokeHTML = '';
   const lkNum = await getLike();
   pokeArr.map((e, i) => {
-    pokeHTML += `<div class="pokemon"><div class="img-container"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokeArr[i].id}.png" class="poke-img"></div><div class="poke-title"><p class="poke-name">${pokeArr[i].name}</p><i class="far fa-heart lk"></i></div><div class="poke-like"><p class="likes"><span class ="lk-counter">${lkNum[i].likes}</span>Likes</p></div><div class="btn-container"><button class="com-btn">Comments</button></div></div>`;
+    pokeHTML += `<div class="pokemon"><div class="img-container"><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokeArr[i].id}.png" class="poke-img"></div><div class="poke-title"><p class="poke-name">${pokeArr[i].name}</p><i class="far fa-heart lk"></i></div><div class="poke-like"><span class ="lk-counter">${lkNum[i].likes}</span><p class="likes">Likes</p></div><div class="btn-container"><button class="com-btn">Comments</button></div></div>`;
     pokedex.innerHTML = pokeHTML;
     return pokeArr;
   });
   const likes = document.querySelectorAll('.lk');
+  const sapns = document.querySelectorAll('.lk-counter');
   likes.forEach((like, i) => {
     like.addEventListener('click', () => {
+      sapns[i].textContent = Number(sapns[i].textContent) + 1;
       postLike(i);
     });
   });
